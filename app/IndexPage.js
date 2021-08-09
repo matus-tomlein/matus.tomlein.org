5bc3d3638105c34a3d13a27fbf81ae42fd25acd7
Matúš Tomlein <matus.tomlein@gmail.com>
2021-08-09 11:21:04 +0200

    Update front image

--- app/IndexPage.js
+++ app/IndexPage.js
@@ -39,7 +39,7 @@ const IndexPage = () => {
 
   return <article>
     <figure>
-      <img src="/images/banner.jpg" />
+      <img src="/images/banner.jpg" style={{ maxWidth: '400px' }} />
     </figure>
 
     <h1>
